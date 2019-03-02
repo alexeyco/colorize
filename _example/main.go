5a82631b749f47dca8f15b7c644a08dde8bea682
Alexey Popov <6425762@gmail.com>
2019-03-02 12:11:53 +0200

    Example fixed

--- _example/main.go
+++ _example/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Column struct {
-	Text string
+	Text  string
 	Style colorize.Style
 }
 
@@ -15,187 +15,183 @@ type Row []Column
 var rows = []Row{
 	{
 		Column{
-			Text: "Black foreground",
+			Text:  "Black foreground",
 			Style: colorize.FgBlack,
 		},
 		Column{
-			Text: "Black background",
+			Text:  "Black background",
 			Style: colorize.BgBlack,
 		},
 		Column{
-			Text: "Dark gray foreground",
+			Text:  "Dark gray foreground",
 			Style: colorize.FgDarkGray,
 		},
 		Column{
-			Text: "Dark gray background",
+			Text:  "Dark gray background",
 			Style: colorize.BgDarkGray,
 		},
 	},
 	{
 		Column{
-			Text: "Red foreground",
+			Text:  "Red foreground",
 			Style: colorize.FgRed,
 		},
 		Column{
-			Text: "Red background",
+			Text:  "Red background",
 			Style: colorize.BgRed,
 		},
 		Column{
-			Text: "Light red foreground",
+			Text:  "Light red foreground",
 			Style: colorize.FgLightRed,
 		},
 		Column{
-			Text: "Light red background",
+			Text:  "Light red background",
 			Style: colorize.BgLightRed,
 		},
 	},
 	{
 		Column{
-			Text: "Green foreground",
+			Text:  "Green foreground",
 			Style: colorize.FgGreen,
 		},
 		Column{
-			Text: "Green background",
+			Text:  "Green background",
 			Style: colorize.BgGreen,
 		},
 		Column{
-			Text: "Light green foreground",
+			Text:  "Light green foreground",
 			Style: colorize.FgLightGreen,
 		},
 		Column{
-			Text: "Light green background",
+			Text:  "Light green background",
 			Style: colorize.BgLightGreen,
 		},
 	},
 	{
 		Column{
-			Text: "Yellow foreground",
+			Text:  "Yellow foreground",
 			Style: colorize.FgYellow,
 		},
 		Column{
-			Text: "Yellow background",
+			Text:  "Yellow background",
 			Style: colorize.BgYellow,
 		},
 		Column{
-			Text: "Light yellow foreground",
+			Text:  "Light yellow foreground",
 			Style: colorize.FgLightYellow,
 		},
 		Column{
-			Text: "Light yellow background",
+			Text:  "Light yellow background",
 			Style: colorize.BgLightYellow,
 		},
 	},
 	{
 		Column{
-			Text: "Blue foreground",
+			Text:  "Blue foreground",
 			Style: colorize.FgBlue,
 		},
 		Column{
-			Text: "Blue background",
+			Text:  "Blue background",
 			Style: colorize.BgBlue,
 		},
 		Column{
-			Text: "Light blue foreground",
+			Text:  "Light blue foreground",
 			Style: colorize.FgLightBlue,
 		},
 		Column{
-			Text: "Light blue background",
+			Text:  "Light blue background",
 			Style: colorize.BgLightBlue,
 		},
 	},
 	{
 		Column{
-			Text: "Magenta foreground",
+			Text:  "Magenta foreground",
 			Style: colorize.FgMagenta,
 		},
 		Column{
-			Text: "Magenta background",
+			Text:  "Magenta background",
 			Style: colorize.BgMagenta,
 		},
 		Column{
-			Text: "Light magenta foreground",
+			Text:  "Light magenta foreground",
 			Style: colorize.FgLightMagenta,
 		},
 		Column{
-			Text: "Light magenta background",
+			Text:  "Light magenta background",
 			Style: colorize.BgLightMagenta,
 		},
 	},
 	{
 		Column{
-			Text: "Cyan foreground",
+			Text:  "Cyan foreground",
 			Style: colorize.FgCyan,
 		},
 		Column{
-			Text: "Cyan background",
+			Text:  "Cyan background",
 			Style: colorize.BgCyan,
 		},
 		Column{
-			Text: "Light cyan foreground",
+			Text:  "Light cyan foreground",
 			Style: colorize.FgLightCyan,
 		},
 		Column{
-			Text: "Light cyan background",
+			Text:  "Light cyan background",
 			Style: colorize.BgLightCyan,
 		},
 	},
 	{
 		Column{
-			Text: "White foreground",
+			Text:  "White foreground",
 			Style: colorize.FgWhite,
 		},
 		Column{
-			Text: "White background",
+			Text:  "White background",
 			Style: colorize.BgWhite,
 		},
 		Column{
-			Text: "Light white foreground",
+			Text:  "Light white foreground",
 			Style: colorize.FgLightWhite,
 		},
 		Column{
-			Text: "Light white background",
+			Text:  "Light white background",
 			Style: colorize.BgLightWhite,
 		},
 	},
 	{
 		Column{
-			Text: "Bold text",
+			Text:  "Bold text",
 			Style: colorize.Bold,
 		},
 		Column{
-			Text: "Fuzzy text",
+			Text:  "Fuzzy text",
 			Style: colorize.Fuzzy,
 		},
 		Column{
-			Text: "Strike text",
+			Text:  "Strike text",
 			Style: colorize.Strikethrough,
 		},
 		Column{
-			Text: "Italic text",
+			Text:  "Italic text",
 			Style: colorize.Italic,
 		},
 	},
 	{
 		Column{
-			Text: "Underscore text",
+			Text:  "Underscore text",
 			Style: colorize.Underscore,
 		},
 		Column{
-			Text: "Blink text",
+			Text:  "Blink text",
 			Style: colorize.Blink,
 		},
 		Column{
-			Text: "Fast blink text",
-			Style: colorize.FastBlink,
-		},
-		Column{
-			Text: "Reverse text",
+			Text:  "Reverse text",
 			Style: colorize.Reverse,
 		},
 	},
 }
 
-func main()  {
+func main() {
 	for _, row := range rows {
 		for _, column := range row {
 			fmt.Print(colorize.Colorize(fmt.Sprintf("%-24v", column.Text), column.Style))
